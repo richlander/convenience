15b224add668ae36bc0eebe9a376b6ea8e8c2883
Richard Lander <rlander@microsoft.com>
2023-10-09 10:04:02 -0700

    Add missing files

---
+++ wordcount/wordcount/BenchmarkTests.cs
@@ -0,0 +1,35 @@
+using BenchmarkDotNet.Attributes;
+
+namespace BenchmarkTests;
+
+[HideColumns("Error", "StdDev", "Median", "RatioSD")]
+[MemoryDiagnoser]
+public class BenchmarkTests
+{
+
+    public string FilePath { get; set; } = BenchmarkData.BenchmarkValues.FilePath;
+
+    [Benchmark]
+    public void FileOpenHandle() => FileOpenHandleBenchmark.FileOpenHandleBenchmark.Count(FilePath);
+
+    [Benchmark]
+    public void FileOpen() => FileOpenBenchmark.FileOpenBenchmark.Count(FilePath);
+
+    [Benchmark]
+    public void FileOpenSearchValues() => FileOpenSearchValuesBenchmark.FileOpenSearchValuesBenchmark.Count(FilePath);
+
+    [Benchmark(Baseline = true)]
+    public void FileOpenSToub() => FileOpenSToubBenchmark.FileOpenSToubBenchmark.Count(FilePath);
+
+    [Benchmark]
+    public void FileOpenSearchValuesSToub() => FileOpenSearchValuesSToubBenchmark.FileOpenSearchValuesSToubBenchmark.Count(FilePath);
+
+    [Benchmark]
+    public void FileOpenText() => FileOpenTextBenchmark.FileOpenTextBenchmark.Count(FilePath);
+
+    [Benchmark]
+    public void FileReadLines() => FileReadLinesBenchmark.FileReadLinesBenchmark.Count(FilePath);
+
+    [Benchmark]
+    public void FileReadAllLines() => FileReadAllLinesBenchmark.FileReadAllLinesBenchmark.Count(FilePath);
+}
