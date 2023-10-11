5b36bff82c6ab3ce8a0f82af104d26f77770a68f
Richard Lander <rlander@microsoft.com>
2023-10-11 09:16:20 -0700

    Add missing files

---
+++ wordcount/wordcount/FileOpenTextReadLineBenchmark.cs
@@ -0,0 +1,35 @@
+
+using BenchmarkData;
+
+namespace FileOpenTextReadLineBenchmark;
+
+public static class FileOpenTextReadLineBenchmark
+{
+    public static Count Count(string path)
+    {
+        int wordCount = 0, lineCount = 0, charCount = 0;
+        using var stream = File.OpenText(path);
+
+        string? line = null;
+        while ((line = stream.ReadLine()) is not null)
+        {
+            lineCount++;
+            charCount += line.Length;
+            bool wasSpace = true;
+
+            foreach (var c in line)
+            {
+                bool isSpace = Char.IsWhiteSpace(c);
+
+                if (!isSpace && wasSpace)
+                {
+                    wordCount++;
+                }
+
+                wasSpace = isSpace;
+            }
+        }
+
+        return new(lineCount, wordCount, charCount, Path.GetFileName(path));
+    }
+}
